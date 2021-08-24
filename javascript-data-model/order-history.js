b456c7a55a3607651d654395deac94e2a3055d15
Simon Kim <mrsimonkim@gmail.com>
2021-08-24 11:16:31 -0700

    javascript-data-model - Simon Kim.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,52 @@
+var orderHistory = [
+  {
+    orderplaced: '080420',
+    numberofitems: 1,
+    totalamount: 34.00,
+    shipto: 'JS Masher',
+    ordernumber: 11439416898772232,
+    delivered: '080820',
+    title: 'JavaScript For Impatient Programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnwindow: '090720',
+    itemprice: 31.55
+  },
+  {
+    orderplaced: '071920',
+    numberofitems: 1,
+    totalamount: 44.53,
+    shipto: 'JS Masher',
+    // eslint-disable-next-line no-loss-of-precision
+    ordernumber: 11399842681280257,
+    delivered: '072020',
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnwindow: '081920',
+    itemprice: 41.33
+  },
+  {
+    orderplaced: '070420',
+    numberofitems: 1,
+    totalamount: 17.22,
+    shipto: 'JS Masher',
+    // eslint-disable-next-line no-loss-of-precision
+    ordernumber: 11428755579059409,
+    delivered: '070720',
+    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. no Driver and No Lag-Gamecube Adapter',
+    author: '',
+    returnwindow: '080520',
+    itemprice: 15.98
+  },
+  {
+    orderplaced: '070520',
+    numberofitems: 2,
+    totalamount: 138.93,
+    shipto: 'JS Masher',
+    ordernumber: 11328831772648248,
+    delivered: '070520',
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)' + 'The Art of Sql',
+    author: '' + 'Faroult, Stephane',
+    returnwindow: '080420',
+    itemprice: 94.95 + 33.99
+  }
+];
