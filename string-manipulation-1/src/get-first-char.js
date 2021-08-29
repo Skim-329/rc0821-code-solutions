11e0cdcce08c18a0fe7fa61cf235859d541b0e5d
Simon Kim <mrsimonkim@gmail.com>
2021-08-28 21:44:50 -0700

    string-manipulation-1 - Simon Kim.

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,9 @@
+/* exported getFirstChar */
+/* Pseudo Code
+Define a function named getFirstChar with a parameter of a string.
+Return the first character of the string.
+*/
+
+function getFirstChar(string) {
+  return string[0];
+}
