05307e6cefba764cf4cdaf11f66504bc96569d22
Simon Kim <mrsimonkim@gmail.com>
2021-08-30 10:58:33 -0700

    dom-querying - Simon Kim.

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+console.log('hello, world');
+var x = document.querySelector('h1');
+console.log(x);
+console.dir(x);
+var y = document.querySelector('#explanation');
+console.log(y);
+console.dir(y);
+var z = document.querySelector('.hint');
+console.log(z);
+console.dir(z);
+var a = document.querySelectorAll('p');
+console.log(a);
+var b = document.querySelectorAll('.example-link');
+console.log(b);
