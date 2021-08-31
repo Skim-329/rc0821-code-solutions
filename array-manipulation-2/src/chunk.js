32aa3f7969dc1ca7dd1a92b0a93fd78d8b5b822f
Simon Kim <mrsimonkim@gmail.com>
2021-08-31 10:09:59 -0700

    array-manipulation-2 - in progress.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,20 +1,12 @@
 /* exported chunk */
-
-/*
 function chunk(array, size) {
   var newArray = [];
-  var sizeArray = [];
-  var subArray = [];
-
-  for (var i = 0; i < size; i++) {
-    var x = array[i];
-    var y =
-    if (array.length > 0) {
-      sizeArray.push(x);
-    } else {
-      sizeArray.push();
+  for (var i = 0; i < array.length; i += size) {
+    var arrayOne = [];
+    for (var x = i; x < size - 1; x++) {
+      arrayOne.push(x);
     }
+    newArray.push(arrayOne);
   }
   return newArray;
 }
-*/
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,8 +1,10 @@
 /* exported includes */
-function includes(array, values) {
-  if (array[array[values]]) {
-    return true;
-  } else {
-    return false;
+function includes(array, value) {
+  var returnBoolean = false;
+  for (var i = 0; i < array.length; i++) {
+    if (value === array[i]) {
+      returnBoolean = true;
+    }
   }
+  return returnBoolean;
 }
