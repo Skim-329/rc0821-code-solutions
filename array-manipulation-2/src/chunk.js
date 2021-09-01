7d056f693577fb9a068c3c4508385e35fcd47941
Simon Kim <mrsimonkim@gmail.com>
2021-08-31 22:12:29 -0700

    array-manipulation-2 - Simon Kim.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -3,8 +3,10 @@ function chunk(array, size) {
   var newArray = [];
   for (var i = 0; i < array.length; i += size) {
     var arrayOne = [];
-    for (var x = i; x < size - 1; x++) {
-      arrayOne.push(x);
+    for (var x = i; x < i + size; x++) {
+      if (x < array.length) {
+        arrayOne.push(array[x]);
+      }
     }
     newArray.push(arrayOne);
   }
