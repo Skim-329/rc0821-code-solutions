bd01a80229c5d073d1c78bab6e1f390da6739580
Simon Kim <mrsimonkim@gmail.com>
2021-09-06 21:51:45 -0700

    string-manipulation-2 - in progress.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,12 @@
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+  var newWord;
+  if (word.toLowerCase() === 'javascript') {
+    newWord = 'JavaScript';
+  } else {
+    var first = word[0].toUpperCase();
+    var second = word.slice(1).toLowerCase();
+    newWord = first + second;
+  }
+  return newWord;
+}
