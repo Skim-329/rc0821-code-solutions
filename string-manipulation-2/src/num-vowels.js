09d1b8c55ba3d8d9c81e9f61934bceaed434ecfe
Simon Kim <mrsimonkim@gmail.com>
2021-09-07 09:45:56 -0700

    string-manipulation-2 - in progress.

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,22 @@
 /* exported numVowels */
+/*
+Define a new function named numVowels with parameter of a string.
+Assign 0 to new variable numberOf.
+Assign lowercased string to new variable lowerCase.
+Loop through each index of lowerCase.
+Assign lowerCase at index to new variable currentLetter.
+Compare currentLetter to list of lower cased vowels.
+Increment numberOf each time there is a vowel.
+Return numberOf.
+*/
+function numVowels(string) {
+  var numberOf = 0;
+  var lowerCase = string.toLowerCase();
+  for (var x = 0; x < lowerCase.length; x++) {
+    var currentLetter = lowerCase[x];
+    if (currentLetter === 'a' || currentLetter === 'e' || currentLetter === 'i' || currentLetter === 'o' || currentLetter === 'u') {
+      numberOf++;
+    }
+  }
+  return numberOf;
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,22 @@
 /* exported ransomCase */
+/*
+Define new function named ransomCase with parameter string.
+Create a new variable lowerCase that is assigned the returned value of string lowercased.
+Capitalize every odd index for the length of the string.
+return string.
+*/
+
+function ransomCase(string) {
+  var lowerCase = string.toLowerCase();
+  var newString = '';
+
+  for (var i = 0; i < lowerCase.length; i++) {
+    var currentLetter = lowerCase[i];
+    if (i % 2 !== 0) {
+      newString += currentLetter.toUpperCase();
+    } else {
+      newString += currentLetter;
+    }
+  }
+  return newString;
+}
