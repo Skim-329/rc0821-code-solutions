b2ae4f52ce9bb4e657aad07716b34a372fc83995
Simon Kim <mrsimonkim@gmail.com>
2021-09-16 09:50:48 -0700

    javascript-bank - in progress.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,15 +1,27 @@
 /* exported Bank */
 function Bank() {
-  this.nextAccountNumber = nextAccountNumber;
+  this.nextAccountNumber = 1;
   this.accounts = [];
 }
 
 Bank.prototype.openAccount = function (holder, balance) {
+  var returnValue = null;
   if ((Number.isInteger(balance)) && (balance > 0)) {
     var newAccount = new Account(this.nextAccountNumber, holder);
-    newAccount.push(balance);
+    newAccount.deposit(balance);
     this.accounts.push(newAccount);
     this.nextAccountNumber++;
+    returnValue = newAccount.number;
   }
-  return this.number;
+  return returnValue;
+};
+
+Bank.prototype.getAccount = function (number) {
+  var returnValue = null;
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts.number[i] === number) {
+      returnValue = i;
+    }
+  }
+  return returnValue;
 };
