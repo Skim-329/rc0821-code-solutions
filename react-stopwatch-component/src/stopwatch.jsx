d5e502597582704f1717ad4109bbb747331fbe9d
Simon Kim <mrsimonkim@gmail.com>
2021-10-22 18:40:49 +0000

    structure complete - adjusting functionality

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,38 +1,47 @@
 import React from 'react';
 
 class StopWatch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { second: 0 };
-    this.startWatch = this.startWatch.bind(this);
+  constructor() {
+    super();
+    this.state = { seconds: 0, watchStart: false, watch: null };
   }
 
   startWatch() {
-    setInterval(() => {
-      return this.setState((state, props) => {
-        return { second: state.second === 59 ? 0 : state.second + 1 };
-      });
-    }, 1000);
+    this.setState({ seconds: this.state.seconds + 1 });
+  }
+
+  handleClick() {
+    if (this.state.watchStart) {
+      clearInterval(this.state.watch);
+    } else {
+      const watch = setInterval(() => this.startWatch(), 1000);
+      this.setState({ watch });
+    }
+    this.setState({ watchStart: !this.state.watchStart });
   }
 
   resetWatch() {
-    return this.setState((state, props) => {
-      return { second: 0 };
-    });
+    if (!this.state.watchStart) {
+      clearInterval(this.state.watch);
+      this.setState({ watch: null, seconds: 0 });
+    }
   }
 
   render() {
+    let icon;
+    if (this.state.watchStart) {
+      icon = 'fas fa-pause';
+    } else {
+      icon = 'fas fa-play';
+    }
     return (
       <div className='container'>
-        <div
-          className='number'
-          onClick={this.resetWatch}>
-          {this.state.second}
-        </div>
-        <div className='icon'
-          onClick={this.startWatch}>
-          <p>hello</p>
-        </div>
+        <button className='circle' onClick={() => this.resetWatch()}>
+          <span className='number'>{this.state.seconds}</span>
+        </button>
+        <button className='icons' onClick={() => this.startWatch()}>
+          <i className={icon}></i>
+        </button>
       </div>
     );
   }
