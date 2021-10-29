852bae0e250b346cc9c7afd2716bd5e8add8edf6
Simon Kim <mrsimonkim@gmail.com>
2021-10-29 01:01:52 +0000

    in progress.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,6 +2,7 @@
 function omit(source, keys) {
   var object = {};
   var propArrays = Object.keys(source);
+
   for (var i = 0; i < propArrays.length; i++) {
     if (!keys.includes(propArrays[i])) {
       object[propArrays] = source[propArrays[i]];
