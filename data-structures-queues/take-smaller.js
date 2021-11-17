d247bbaa8f60da9d18f1a697da2ef5d5c93c8f34
Simon Kim <mrsimonkim@gmail.com>
2021-11-17 22:55:52 +0000

    in progress.

--- data-structures-queues/take-smaller.js
+++ data-structures-queues/take-smaller.js
@@ -1,15 +1,27 @@
 /* exported takeSmaller */
 
+/*
+compare first and second elements.
+
+if first is smaller, return first
+assign second to variable with dequeue
+enqueue second to queue.
+
+else if second is smaller, return second
+assign first to variable with dequeue
+enqueue first to queue.
+*/
+
 function takeSmaller(queue) {
-  // if (queue.peek() !== undefined) {
-  //   const a = queue.dequeue();
-  //   const b = queue.peek();
-  //   queue.dequeue();
-  //   queue.peek();
-  //   if (a < b) {
-  //     return queue.enqueue(a);
-  //   }
-  // } else {
-  //   return;
-  // }
+  if (queue.peek() !== undefined) {
+    const first = queue.peek();
+    const second = queue.peek();
+    if (first < second) {
+      queue.enqueue(second);
+      return first;
+    } else if (first > second) {
+      queue.enqueue(first);
+      return second;
+    }
+  }
 }
